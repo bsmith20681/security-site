82ada3b2d44f1877878a9c4ae387a61a7dab0b6c
bsmith20681 <bsmith20681@gmail.com>
2020-04-04 15:13:13 -0600

    test

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,120 @@
 import React from "react"
+import { Link } from 'gatsby'
 
 const Footer = () => {
-  return <p>Copyright 2020 this is the footer</p>
+  return (
+    <footer>
+      <div className="container">
+        <div className="row footer">
+          <div className="col-md-12">
+            <h1>Logo</h1>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+            <h4>Guides</h4>
+            <ul className="footer-item-list">
+              <li className="footer-item">
+                <Link>
+                  Article 1
+                </Link>
+              </li>
+              <li className="footer-item">
+                <Link>
+                  Article 2
+                </Link>
+              </li>
+              <li className="footer-item">
+                <Link>
+                  Article 3
+                </Link>
+              </li>
+              <li className="footer-item">
+                <Link>
+                  Article 4
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div className="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+            <h4>Guides</h4>
+            <ul className="footer-item-list">
+              <li className="footer-item">
+                <Link>
+                  Article 1
+                </Link>
+              </li>
+              <li className="footer-item">
+                <Link>
+                  Article 2
+                </Link>
+              </li>
+              <li className="footer-item">
+                <Link>
+                  Article 3
+                </Link>
+              </li>
+              <li className="footer-item">
+                <Link>
+                  Article 4
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div className="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+            <h4>Guides</h4>
+            <ul className="footer-item-list">
+              <li className="footer-item">
+                <Link>
+                  Article 1
+                </Link>
+              </li>
+              <li className="footer-item">
+                <Link>
+                  Article 2
+                </Link>
+              </li>
+              <li className="footer-item">
+                <Link>
+                  Article 3
+                </Link>
+              </li>
+              <li className="footer-item">
+                <Link>
+                  Article 4
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div className="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+            <h4>Guides</h4>
+            <ul className="footer-item-list">
+              <li className="footer-item">
+                <Link>
+                  Article 1
+                </Link>
+              </li>
+              <li className="footer-item">
+                <Link>
+                  Article 2
+                </Link>
+              </li>
+              <li className="footer-item">
+                <Link>
+                  Article 3
+                </Link>
+              </li>
+              <li className="footer-item">
+                <Link>
+                  Article 4
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+  )
 }
 
 export default Footer
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,33 @@
 import React from "react"
+import { Link } from "gatsby"
 
 import "../styles/index.scss"
 
 const Header = () => {
   return (
     <header>
-      <h1>This is the header</h1>
+      <div className="container">
+        <div className="row nav">
+          <h1>Logo</h1>
+          <ul className="nav-item-list">
+            <li className="nav-item">
+              <Link>
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link>
+                Blog
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link>
+                About
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
     </header>
   )
 }
