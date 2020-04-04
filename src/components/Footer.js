f69820df4d9bf2905cb4886f5d55936123f44247
Ben <bsmith20681@gmail.com>
2020-04-03 21:17:01 -0600

    second commit

---
+++ src/components/Footer.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const Footer = () => {
+  return <p>Copyright 2020 this is the footer</p>
+}
+
+export default Footer
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,15 @@
 import React from "react"
+import { Link } from "gatsby"
 
-export default () => <div>Hello world!</div>
+import Layout from '../components/Layout'
+
+const Index = () => {
+  return (
+    <Layout>
+    <h1>Hello World!</h1>
+    <Link to="/about">Go to about page</Link>
+    </Layout>
+  )
+}
+
+export default Index;
