d8803fb32651920be86dfbf326c5a3c3888e786e
Ben <bsmith20681@gmail.com>
2020-04-06 20:44:07 -0600

    finished html for home and about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,78 @@
 import React from "react"
 
+import profilePic from "../images/your-home-security-expert.com.png"
+import signature from "../images/signature.png"
+
 import Layout from "../components/Layout"
 
 const About = () => {
   return (
     <Layout>
-      <h1>This is the about page</h1>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12 text-center">
+            <h2>
+              Hi, I'm Ben and I am here to help you find the right security
+              system for your home
+            </h2>
+            <p>
+              I have worked in the home security industry for over 5 years now.
+              On this website I give you the inside scoop on all the security
+              companies.
+            </p>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-8 col-sm-12">
+            <p>
+              Searching for the right security system can be a daunting task.
+              Home security systems have been around for a while now and as a
+              result there are too many options. My goal is to make deciding the
+              best home security system for your home an easier task. With my
+              five years in the industry, I have categorized all the options
+              into three simple categories. This will serve as a good starting
+              place because what may be a good fit for your neighbor may not be
+              a good fit for you.
+            </p>
+            <p>
+              I have reviews, comparisons, and help insider tips on this website
+              to make sure you are making the right decision. I shoot it
+              straight. You will not find any fluff in my articles. I try my
+              best to make complicated concepts easy to understand.
+            </p>
+          </div>
+          <div className="col-md-4 col-sm-12">
+            <img
+              src={profilePic}
+              alt="picture of ben from yourhomesecurityexpert.com"
+              style={{ width: 326 }}
+            />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-12">
+            <p>
+              My work is supported through affiliate links. When someone clicks
+              on a link and uses the service I recommended, I receive a
+              commission. However, I do not let the size of the commission
+              influence my over grade. My commitment to towards honesty and
+              being as straight forward as possible.
+            </p>
+            <p>
+              When I'm not working on this site, you can find me going to the
+              movies, flying my drone, and watching the next UFC fight. If you
+              feel like you need some extra help you can email me directly at
+              ben@yourhomesecurityexpert.com.
+            </p>
+            <p>
+              Thanks
+              <br />
+              -Ben Smith
+            </p>
+            <img src={signature} alt="signature ben smith" />
+          </div>
+        </div>
+      </div>
     </Layout>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,118 @@
 import React from "react"
 import { Link } from "gatsby"
 
+import profilePic from "../images/your-home-security-expert.com.png"
+
+import cardStyles from "../styles/components/card.module.scss"
+
 import Layout from "../components/Layout"
 
 const Index = () => {
   return (
     <Layout>
-      <h1>Hello World!</h1>
-      <Link to="/about">Go to about page</Link>
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-8 col-md-8 col-sm-12 col-xs-12">
+            <h1>Find the Right Home Security System is.... Confusing</h1>
+            <p>
+              My name is Ben and I'm here to help. I've been in the home
+              security industry for over 5 years now. I review the best home
+              security companies in the industry. I'm here to give you the
+              <strong>insider scoop</strong> so you can choose the
+              <strong>right home security system</strong> for your home.
+            </p>
+            <div className="row">
+              <Link>
+                <button>Get Started</button>
+              </Link>
+              <Link to="/about">
+                <button>About Me</button>
+              </Link>
+            </div>
+          </div>
+          <div className="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+            <img
+              src={profilePic}
+              alt="picture of ben from yourhomesecurityexpert.com"
+            />
+          </div>
+        </div>
+        <hr />
+      </div>
+      <div className="container">
+        <div className="row text-center">
+          <div className="col-md-12">
+            <h1>Stop the Endless Searching Today</h1>
+            <p>
+              Let's face it shopping for the right security system is time
+              consuming and exhausting! There are so many options which is why
+              it makes it so difficult to decide.
+            </p>
+            <p>
+              I have organized the majority of home security options into three
+              simple categories to get you started.
+            </p>
+          </div>
+        </div>
+        <div className={`row ${cardStyles.cardContainer}`}>
+          <div
+            className={`col-lg-3 col-md-3 col-sm-12 col-xs-12 ${cardStyles.card}`}
+          >
+            <h4>Smart Home Security</h4>
+            <p>
+              Lights, cameras, thermostats, garage door, voice control, and
+              security monitoring. This is what I recommend.
+            </p>
+            <p>
+              <em>Examples: Vivint, ADT</em>
+            </p>
+          </div>
+          <div
+            className={`col-lg-3 col-md-3 col-sm-12 col-xs-12 ${cardStyles.card}`}
+          >
+            <h4>Standard Home Security</h4>
+            <p>
+              No fancy bells and whistles. Just affordable, and reliable home
+              security with professional installation. This is what I recommend.
+            </p>
+            <p>
+              <em>Examples: CPI Security, Protect America, Monotronics</em>
+            </p>
+          </div>
+          <div
+            className={`col-lg-3 col-md-3 col-sm-12 col-xs-12 ${cardStyles.card}`}
+          >
+            <h4>DIY Security</h4>
+            <p>
+              The do-it-yourself security system. Save money by installing it on
+              your own. This is what I recommend.
+            </p>
+            <p>
+              <em>Examples: Simplisafe, Frontpoint, Ring Alarm System</em>
+            </p>
+          </div>
+        </div>
+        <div className="row text-center">
+          <div className="col-lg-12 col-md-12 col-xs-12">
+            <h2>What if I only want Cameras?</h2>
+            <p>
+              Some people don't want the monthly fee. They just want something
+              they can monitor from their phone from any where.
+            </p>
+            <p>
+              Cameras are a great option to capture what happens and have solid
+              video evidence that can be shown to the police.
+            </p>
+            <p>
+              Click on the button below to see what options I recommend for best
+              outside security cameras.
+            </p>
+            <Link>
+              <button>See All Camera Options</button>
+            </Link>
+          </div>
+        </div>
+      </div>
     </Layout>
   )
 }
